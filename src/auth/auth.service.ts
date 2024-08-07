a4c770b5d401025015d92946477d57a5a39a1488
CertBR1 <mateus.ribeiro3591@gmail.com>
2024-08-07 18:59:21 -0300

    atualizado logins por email

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -64,7 +64,7 @@ export class AuthService {
         throw new HttpException('Código inválido', 401);
       }
       const cliente = await this.clienteRepository.findOne({
-        where: { email: codeDto.email },
+        where: codeDto.email ? { email: codeDto.email } : { whatsapp: codeDto.whatsapp },
       })
       console.log('Auth service: validarCodigo cliente encontrado', cliente);
 
